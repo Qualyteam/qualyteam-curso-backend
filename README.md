9a84e9526a8b977e3db27f5b0520d56f934ac12f
Leonardo Prange <leonardoprange@gmail.com>
2020-02-14 14:52:49 -0300

    Criacao estrutura inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+Dependencias WebApi
+
+https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.SqlServer  
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 3.1.1
---
+++ ReceitasWebApi/Infrastructure/Context.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ReceitasWebApi.Infrastructure
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
