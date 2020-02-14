5b3c21b129338bf00bcdee038c9a5f7cd17b9b23
Leonardo Prange <leonardoprange@gmail.com>
2020-02-14 15:06:07 -0300

    Implement controller base

---
+++ ReceitasWebApi/Controllers/ReceitaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ReceitasWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReceitaController : ControllerBase
+    {
+        public ReceitaController()
+        { }
+
+        [HttpGet]
+        public async Task GetAllAsync()
+        { }
+
+        [HttpPost]
+        public async Task InsertAsync()
+        { }
+
+        [HttpPut("{id:guid}")]
+        public async Task UpdateAsync()
+        { }
+    }
+
+}
